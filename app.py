a5d005b918f4b26efa064f961c79063f1b7fe288
Bobesh135 <tomiseknosekyt@gmail.com>
2025-10-27 17:45:28 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.secret_key = 'super-tajny-klic' 
+app.secret_key = 'super-tajny-klic'
 
 # Heslo zůstává stejné
 CORRECT_PASSWORD = "1234"
@@ -12,23 +12,19 @@ def login():
     if request.method == 'POST':
         password = request.form.get('password')
         if password == CORRECT_PASSWORD:
-            # Po správném zadání hesla přesměrujeme na novou funkci 'dashboard'
             return redirect(url_for('dashboard'))
         else:
             error = "Nesprávné heslo, zkus to znovu."
-            
+
     return render_template('index.html', error=error)
 
 @app.route('/dashboard')
 def dashboard():
-    # Zde si připravíme fiktivní data pro zobrazení na stránce.
-    # Později je nahradíme daty z databáze.
+    # Odebrali jsme data pro 'owe'
     transaction_data = {
         'owned': 29375,
-        'landed': 4000,
-        'owe': 1135
+        'landed': 4000
     }
-    # Načteme nový HTML soubor a předáme mu data
     return render_template('dashboard.html', data=transaction_data)
 
 if __name__ == '__main__':
